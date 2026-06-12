9881cab14436ebb2eac5dcce916fdedb5318e392
embersongs <67457948+embersongs@users.noreply.github.com>
2026-06-12 18:34:19 +0500

    Cucumber
    Человек и огурец

--- Assets/Scenes/RotateObject.cs
+++ Assets/Scenes/RotateObject.cs
@@ -8,15 +8,10 @@ public class RotateObject : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.O))
-        {
+   
             // Вращение влево
             transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.P))
-        {
-            // Вращение вправо
-            transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
-        }
+    
+
     }
 }
